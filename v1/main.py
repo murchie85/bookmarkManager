a8a7982413b604565fe4b0523ee949111977483c
murchie85 <murchie85@gmail.com>
2022-12-05 21:25:02 +0000

    updating version 2

--- v1/main.py
+++ v1/main.py
@@ -6,38 +6,30 @@ app = Flask(__name__)
 def init_db():
     # Connect to the database and create the bookmarks table if it does not exist
     conn = sqlite3.connect('bookmarks.db')
-    conn.execute("CREATE TABLE IF NOT EXISTS bookmarks (name TEXT, url TEXT, category TEXT)")
+    conn.execute("CREATE TABLE IF NOT EXISTS bookmarks (name TEXT, url TEXT)")
     conn.commit()
 
 @app.route('/')
 def home():
     # Connect to the database and get the bookmark list
     conn = sqlite3.connect('bookmarks.db')
-    cursor = conn.execute("SELECT name, url, category FROM bookmarks")
-    bookmarks = [{'name': name, 'url': url, 'category': category} for name, url, category in cursor]
-
-    # Group the bookmarks by category
-    categories = {}
-    for bookmark in bookmarks:
-        if bookmark['category'] not in categories:
-            categories[bookmark['category']] = []
-        categories[bookmark['category']].append(bookmark)
+    cursor = conn.execute("SELECT name, url FROM bookmarks")
+    bookmarks = [{'name': name, 'url': url} for name, url in cursor]
 
     # Render the bookmark list on the home page
-    return render_template('home.html', categories=categories)
+    return render_template('home.html', bookmarks=bookmarks)
 
 @app.route('/add', methods=['POST'])
 def add_bookmark():
-    # Get the name, URL, and category of the bookmark from the form
+    # Get the name and URL of the bookmark from the form
     name = request.form['name']
     url = request.form['url']
-    category = request.form['category']
 
     # Connect to the database and add the bookmark if it does not already exist
     conn = sqlite3.connect('bookmarks.db')
     cursor = conn.execute("SELECT * FROM bookmarks WHERE name=? AND url=?", (name, url))
     if cursor.fetchone() is None:
-        conn.execute("INSERT INTO bookmarks VALUES (?, ?, ?)", (name, url, category))
+        conn.execute("INSERT INTO bookmarks VALUES (?, ?)", (name, url))
         conn.commit()
 
     # Redirect to the home page
---
+++ v2/app.py
@@ -0,0 +1,107 @@
+from flask import Flask, request, redirect, render_template
+import sqlite3
+
+# create the Flask app
+app = Flask(__name__)
+
+# connect to the SQLite database
+def connect_db():
+    return sqlite3.connect('bookmarks.db')
+
+# create the bookmarks table in the database
+def create_table():
+    db = connect_db()
+    cursor = db.cursor()
+    cursor.execute("""
+        CREATE TABLE bookmarks (
+            id INTEGER PRIMARY KEY,
+            name TEXT,
+            category TEXT,
+            url TEXT
+        )
+    """)
+    db.commit()
+
+# add a new bookmark to the database
+def add_bookmark(name, category, url):
+    db = connect_db()
+    cursor = db.cursor()
+    cursor.execute("""
+        INSERT INTO bookmarks (name, category, url)
+        VALUES (?, ?, ?)
+    """, (name, category, url))
+    db.commit()
+
+# delete a bookmark from the database
+def delete_bookmark(id):
+    db = connect_db()
+    cursor = db.cursor()
+    cursor.execute("""
+        DELETE FROM bookmarks WHERE id = ?
+    """, (id,))
+    db.commit()
+
+# get a list of all bookmarks from the database
+def get_bookmarks():
+    db = connect_db()
+    cursor = db.cursor()
+    cursor.execute("""
+        SELECT * FROM bookmarks
+    """)
+    return cursor.fetchall()
+
+# handle requests to the homepage
+@app.route('/')
+def index():
+    # get a list of all bookmarks
+    bookmarks = get_bookmarks()
+
+    # group bookmarks by category
+    categories = {}
+    for bookmark in bookmarks:
+        id, name, category, url = bookmark
+        if category not in categories:
+            categories[category] = []
+        categories[category].append((id, name, url))
+
+    return render_template('index.html', categories=categories)
+
+# handle requests to add a new bookmark
+@app.route('/add', methods=['POST'])
+def add():
+    # get the user's input
+    name = request.form['name']
+    category = request.form['category']
+    url = request.form['url']
+
+    # add the bookmark to the database
+    add_bookmark(name, category, url)
+
+    # redirect the user back to the homepage
+    return redirect('/')
+
+@app.route('/style.css')
+def css():
+    return app.send_static_file('style.css')
+
+
+# handle requests to delete a bookmark
+@app.route('/delete/<id>', methods=['POST'])
+def delete(id):
+    # delete the bookmark from the database
+    delete_bookmark(id)
+
+    # redirect the user back to the homepage
+    return redirect('/')
+
+
+
+# create the database and table if they don't exist
+try:
+    create_table()
+except sqlite3.OperationalError:
+    pass
+
+# run the app
+if __name__ == '__main__':
+    app.run()
